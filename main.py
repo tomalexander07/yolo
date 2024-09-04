82d10eefba65536491a6318defad7238c2f38cb8
Ayush Sharma <157779439+tomalexander07@users.noreply.github.com>
2024-09-04 23:12:19 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+from ultralytics import solutions
+
+# Pass a model as an argument
+solutions.inference(model="D:\\cvit\\Testing\\runs\\detect\\train\\weights\\best.pt")
+
+### Make sure to run the file using command `streamlit run <file-name.py>`
