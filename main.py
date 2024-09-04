7d31a25aa4704a3d83d1f3966b3a094c7c398eb0
Ayush Sharma <157779439+tomalexander07@users.noreply.github.com>
2024-09-04 23:15:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from ultralytics import solutions
 
 # Pass a model as an argument
-solutions.inference(model="D:\\cvit\\Testing\\runs\\detect\\train\\weights\\best.pt")
+solutions.inference(model="yolov8n.pt")
 
 ### Make sure to run the file using command `streamlit run <file-name.py>`
